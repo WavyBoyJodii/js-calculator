e9a3b77de739b2ccd3a9354a7e33abc40e7e1c76
WavyBoyJodii <najeecasey@gmail.com>
2022-10-28 22:20:48 -0400

    Add clear functionality revamped class for buttons

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 const display = document.querySelector(".display");
+const clearButton = document.querySelector(".clear");
 
 const one = document.getElementById("one");
 const two = document.getElementById("two");
@@ -35,6 +36,9 @@ seven.addEventListener('click', () => press(7));
 eight.addEventListener('click', () => press(8));
 nine.addEventListener('click', () => press(9));
 zero.addEventListener('click', () => press(0));
+decimalButton.addEventListener('click', () => pressForDec("."));
+
+clearButton.addEventListener('click', () => clearDisplay());
 
 function add(x,y) {
     let total = x+y;
@@ -83,7 +87,63 @@ function updateDisplay() {
     display.textContent = numberBuilder.toString().replace(/,/g,"");
 }
 
+function checkNumLength() {
+    if (numberBuilder.length > 7) {
+        display.style.fontSize = "35px";
+    }
+
+    if (numberBuilder.length > 13) {
+        display.style.fontSize = "25px";
+    }
+
+    if (numberBuilder.length > 21) {
+        display.style.fontSize = "15px";
+    }
+    if (numberBuilder.length > 36) {
+        one.removeEventListener('click',() => press());
+        two.removeEventListener('click', () => press());
+        three.removeEventListener('click', () => press());
+        four.removeEventListener('click', () => press());
+        five.removeEventListener('click', () => press());
+        six.removeEventListener('click', () => press());
+        seven.removeEventListener('click', () => press());
+        eight.removeEventListener('click', () => press());
+        nine.removeEventListener('click', () => press());
+        zero.removeEventListener('click', () => press());
+        
+        one.classList.remove("numbers-hover");
+        two.classList.remove("numbers-hover");
+        three.classList.remove("numbers-hover");
+        four.classList.remove("numbers-hover");
+        five.classList.remove("numbers-hover");
+        six.classList.remove("numbers-hover");
+        seven.classList.remove("numbers-hover");
+        eight.classList.remove("numbers-hover");
+        nine.classList.remove("numbers-hover");
+        zero.classList.remove("numbers-hover");
+        decimalButton.classList.remove("numbers-hover");
+    }
+}
+
+function clearDisplay() {
+    numberBuilder = [];
+    updateDisplay();
+    display.textContent = firstValue;
+}
 function press(x) {
     numberPush(x);
+    checkNumLength();
     updateDisplay();
+}
+
+function pressForDec(x) {
+    numberPush(x);
+    checkNumLength();
+    updateDisplay();
+    removeDecimal();
+    decimalButton.classList.remove("numbers-hover");
+}
+
+function removeDecimal() {
+    decimalButton.removeEventListener('click', () => pressForDec());
 }
